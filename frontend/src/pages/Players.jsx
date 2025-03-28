a19338b64a2d1d1e4b294b645d6502325886ad33
Silas <writer.silas@gmail.com>
2025-03-28 19:18:12 +0300

    players, player search

--- frontend/src/pages/Players.jsx
+++ frontend/src/pages/Players.jsx
@@ -1,8 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "../styles/players.css";
+import { usePlayers } from "../store";
 
 const Players = () => {
+  const [search, setSearch] = useState("");
+  const { players, loadPlayers, searchPlayer } = usePlayers();
+
+  useEffect(() => {
+    loadPlayers();
+  }, []);
+
+  useEffect(() => {
+    const delaySearch = setTimeout(() => {
+      searchPlayer(search || ""); // Ensure empty search resets list
+    }, 300);
+
+    return () => clearTimeout(delaySearch);
+  }, [search]);
+
   return (
-    <div className="h-full w-full flex flex-col items-center">Players</div>
+    <div className="h-full w-full flex flex-col items-center">
+      <div className="w-full h-1/3 hero-players flex items-center justify-start">
+        <div className="flex-1 w-full flex justify-center">
+          <span className="titles !text-white">Players</span>
+        </div>
+        <div className="flex-1">
+          <input
+            type="text"
+            placeholder="Search player..."
+            className="input h-10 rounded-xl shadow-sm"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className=" w-full h-full">
+        <table className="table">
+          {/* head */}
+          <thead className="relative">
+            <tr>
+              <th>Player</th>
+              <th>Position</th>
+              <th>Nationality</th>
+            </tr>
+          </thead>
+          <tbody>
+            {players?.map((player) => (
+              <tr key={player.id}>
+                <td>
+                  <span className="flex items-center">
+                    <img
+                      src={player.image_url}
+                      className="rounded-2xl h-12 w-12"
+                    />
+                    {player.name}
+                  </span>
+                </td>
+                <td>{player.position}</td>
+                <td>{player.nationality}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 };
 
--- frontend/src/store/Players.js
+++ frontend/src/store/Players.js
@@ -1,16 +1,27 @@
 import { create } from "zustand";
 import { ApiErrors, fetchPlayers } from "../services";
 
-export const usePlayers = create((set) => ({
+export const usePlayers = create((set, get) => ({
   players: [],
   loadPlayers: async () => {
-    set({ players: JSON.parse(localStorage.getItem("players")) });
+    // set({ players: JSON.parse(localStorage.getItem("players")) });
     try {
-      const { data } = await fetchPlayers();
+      const data = await fetchPlayers();
       set({ players: data });
       localStorage.setItem("players", JSON.stringify(data));
     } catch (error) {
       ApiErrors(error);
+    }
+  },
+  searchPlayer: (player) => {
+    const storedPlayers = JSON.parse(localStorage.getItem("players")) || [];
+    if (!player.trim()) {
+      set({ players: storedPlayers }); // ✅ Reset when search is empty
+    } else {
+      const searched = storedPlayers.filter((it) =>
+        it.name.toLowerCase().includes(player.toLowerCase())
+      );
+      set({ players: searched });
     }
   },
 }));
